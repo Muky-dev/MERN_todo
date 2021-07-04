cfc927ff04ff642a75bf4ca2c968c2e7b9a17935
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-04 15:09:35 -0300

    Delete to do function

--- Server/src/controllers/todos/index.ts
+++ Server/src/controllers/todos/index.ts
@@ -61,3 +61,23 @@ const updateTodo = async (req: Request, res: Response): Promise<void> => {
         throw error;
     }
 }
+
+const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
+            req.params.id
+        );
+
+        const allTodos: ITodo[] = await Todo.find();
+
+        res
+            .status(200)
+            .json({
+                message: "Todo deleted",
+                todo: deletedTodo,
+                todos: allTodos
+            });
+    } catch (error) {
+        throw error;
+    }
+}

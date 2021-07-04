1521c31532f2987cc11e3447567260cefc5c5000
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-04 15:08:45 -0300

    Add new to do function

--- Server/src/controllers/todos/index.ts
+++ Server/src/controllers/todos/index.ts
@@ -10,3 +10,29 @@ const getTodos = async (req: Request, res: Response): Promise<void> => {
         throw error;
     }
 }
+
+const addTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const body = req.body as Pick<ITodo, "name" | "description" | "status">;
+        
+        const todo: ITodo = new Todo({
+            name: body.name,
+            description: body.description,
+            status: body.status,
+        });
+
+        const newTodo: ITodo = await todo.save();
+        const allTodos: ITodo[] = await Todo.find();
+
+        res
+            .status(201)
+            .json({
+                message: "Todo added",
+                todo: newTodo,
+                todos: allTodos,
+            });
+
+    } catch (error) {
+        throw error;
+    }
+}

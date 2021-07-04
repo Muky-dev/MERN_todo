d392cb8e728ccc037d1aa696a4b0741bec93f312
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-04 15:09:10 -0300

    Update to do function

--- Server/src/controllers/todos/index.ts
+++ Server/src/controllers/todos/index.ts
@@ -36,3 +36,28 @@ const addTodo = async (req: Request, res: Response): Promise<void> => {
         throw error;
     }
 }
+
+
+const updateTodo = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const {
+            params: { id },
+            body,
+        } = req
+
+        const updateTodo: ITodo | null = await Todo.findByIdAndUpdate(
+            { _id: id },
+            body
+            );
+        const allTodos: ITodo[] = await Todo.find();
+        res
+            .status(200)
+            .json({
+                message: "Todo updated",
+                todo: updateTodo,
+                todos: allTodos,
+            });
+    } catch (error) {
+        throw error;
+    }
+}

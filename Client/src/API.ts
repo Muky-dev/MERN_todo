89c0089709fd828b82de897ac224d7580857e629
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-05 20:39:54 -0300

    Add POST to do function

--- Client/src/API.ts
+++ Client/src/API.ts
@@ -5,9 +5,26 @@ const baseUrl: string = "http://localhost:4000";
 export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
     try {
         const todos: AxiosResponse<ApiDataType> = await axios.get(
-            baseUrl + `/todos`
+            baseUrl + "/todos"
         );
         return todos
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+export const addTodo = async (formData: ITodo): Promise<AxiosResponse<ApiDataType>> => {
+    try {
+        const todo: Omit<ITodo, "_id"> = {
+            name: formData.name,
+            description: formData.description,
+            status: false,
+        }
+        const saveTodo: AxiosResponse<ApiDataType> = await axios.post(
+            baseUrl + "/add-todo",
+            todo
+        );
+        return saveTodo
     } catch (error) {
         throw new Error(error);
     }

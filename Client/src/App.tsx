542abfccc323718c9bad6f7e75bfe81228044bc9
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-08 14:05:20 -0300

    Main React File updated

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import TodoItem from './components/TodoItem';
+import AddTodo from './components/AddTodo';
+import { getTodos, addTodo, updateTodo, deleteTodo } from './API';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App : React.FC = () => {
+    const [todos, setTodos] = useState<ITodo[]>([]);
+
+    useEffect(() => {
+        fetchTodos()
+    }, []);
+
+    const fetchTodos = (): void => {
+        getTodos()
+        .then(({ data: { todos } }: ITodo[] | any) => setTodos(todos))
+        .catch((err: Error) => console.log(err))
+    }
 }
-
-export default App;

020bc038e7941ff560c88e93be1bf3c018655846
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-07-09 12:43:31 -0300

    Url enconded fixed

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -8,6 +8,8 @@ const app: Express = express();
 const PORT: string | number = process.env.PORT || 4000;
 
 app.use(cors());
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
+app.use(express.json());
 app.use(todoRoutes);
 
 const uri: string = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.cjwpt.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
